e5e30a048956053ce23a62c4683360ac575133e0
Sabro98 <inhyuk04@gmail.com>
2021-02-15 14:34:35 +0900

    add basket method

--- macro.py
+++ macro.py
@@ -9,15 +9,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementNotInteractableException
 
 class Macro:
-    def __init__(self, stu_no, pw, grade, index):
-        self.driver = webdriver.Chrome(os.path.join(os.getcwd(), 'chromedriver'))
+    def __init__(self, stu_no, pw, grade, index, method=0):
+        #self.driver = webdriver.Chrome(os.path.join(os.getcwd(), 'chromedriver'))
+        self.driver = webdriver.Chrome('chromedriver.exe')
 
         self.grade = grade
         self.index = index
-
+        self.method = method
         self.stu_no = stu_no
         self.pw = pw
-
 
     def refresh(self, spinner1, spinner2):
         spinner1.click()
@@ -51,6 +51,16 @@ class Macro:
         self.driver.find_element_by_id(final_okay_id).click()
         sleep(0.3)
 
+    def getCondition(self):
+        current_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.index) + \
+                     '_cell_' + str(self.index) + '_7GridCellTextSimpleContainerElement'
+        total_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.index) + \
+                   '_cell_' + str(self.index) + '_8GridCellTextSimpleContainerElement'
+
+        current = self.driver.find_element_by_id(current_id)
+        total = self.driver.find_element_by_id(total_id)
+        return current, total
+
     def run(self):
         self.driver.implicitly_wait(3)
         self.driver.get('http://all.jbnu.ac.kr/jbnu/sugang/')
@@ -64,12 +74,11 @@ class Macro:
         login_xpath = '//*[@id="mainframe_VFrameSet_LoginFrame_form_div_login_div_form_btn_login"]/div'
         login = self.driver.find_element_by_xpath(login_xpath)
 
-        
         stu_no.click()
         stu_no.send_keys(self.stu_no)
         pw.click()
         pw.send_keys(self.pw)
-        login.click()       
+        login.click()
 
         sugang_xpath = '//*[@id="mainframe_VFrameSet_TopFrame_form_div_top_mnu_topmenu_0001TextBoxElement"]/div'
 
@@ -78,7 +87,7 @@ class Macro:
             sugang = self.driver.find_element_by_xpath(sugang_xpath)
             sugang.click()
             sleep(0.5)
-            self.inputCode()
+            # self.inputCode()
 
         except ElementNotInteractableException:
             ok_xpath = '//*[@id="mainframe_VFrameSet_LoginFrame_COM_ALERT_form_btn_closeTextBoxElement"]'
@@ -87,41 +96,63 @@ class Macro:
             login.click()
             sugang = self.driver.find_element_by_xpath(sugang_xpath)
             sugang.click()
-            self.inputCode()
+            # self.inputCode()
 
-        spinner1_xpath = '//*[@id="mainframe_VFrameSet_WorkFrame_form_div_work_div_search_cbo_shyr"]/div'
-        spinner1 = self.driver.find_element_by_xpath(spinner1_xpath)
-        spinner1.click()
+        if self.method == 0:
+            spinner1_xpath = '//*[@id="mainframe_VFrameSet_WorkFrame_form_div_work_div_search_cbo_shyr"]/div'
+            spinner1 = self.driver.find_element_by_xpath(spinner1_xpath)
+            spinner1.click()
 
-        spinner2_xpath = '//*[@id="mainframe_VFrameSet_WorkFrame_form_div_work_div_search_cbo_shyr_comboedit_input"]'
-        spinner2 = self.driver.find_element_by_xpath(spinner2_xpath)
-        for _ in range(self.grade + 1):
-            spinner2.send_keys(Keys.DOWN)
-            spinner2.send_keys(Keys.RETURN)
+            spinner2_xpath = '//*[@id="mainframe_VFrameSet_WorkFrame_form_div_work_div_search_cbo_shyr_comboedit_input"]'
+            spinner2 = self.driver.find_element_by_xpath(spinner2_xpath)
+            for _ in range(self.grade + 1):
+                spinner2.send_keys(Keys.DOWN)
+                spinner2.send_keys(Keys.RETURN)
+            current, total = self.getCondition()
+            cnt = 0
+            while total.text == current.text:
+                self.refresh(spinner1, spinner2)
+                sleep(0.5)
+                current, total = self.getCondition()
+                cnt += 1
+                if cnt == 150:
+                    self.driver.quit()
+                    return True
 
-        current_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.index) + \
-                     '_cell_' + str(self.index) + '_7GridCellTextSimpleContainerElement'
-        total_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.index) + \
-                   '_cell_' + str(self.index) + '_8GridCellTextSimpleContainerElement'
-
-        current = self.driver.find_element_by_id(current_id)
-        total = self.driver.find_element_by_id(total_id)
-        cnt = 0
-        while total.text == current.text:
-            self.refresh(spinner1, spinner2)
-            sleep(0.5)
-            current = self.driver.find_element_by_id(current_id)
-            total = self.driver.find_element_by_id(total_id)
-            cnt += 1
-            if cnt == 150:
-                self.driver.quit()
+            if total.text != current.text:
+                btn_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.index) + \
+                         '_cell_' + str(self.index) + '_0_controlbuttonTextBoxElement'
+                btn = self.driver.find_element_by_id(btn_id)
+                btn.click()
+                return False
+            else:
                 return True
 
-        if total.text != current.text:
-            btn_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.index) + \
-                     '_cell_' + str(self.index) + '_0_controlbuttonTextBoxElement'
-            btn = self.driver.find_element_by_id(btn_id)
-            btn.click()
-            return False
         else:
-            return True
+            basket_btn_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_btn_rsrvCourTextBoxElement'
+            other_btn_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_btn_milTextBoxElement'
+
+            basket_btn = self.driver.find_element_by_id(basket_btn_id)
+            other_btn = self.driver.find_element_by_id(other_btn_id)
+            basket_btn.click()
+
+            current, total = self.getCondition()
+            cnt = 0
+            while total.text == current.text:
+                other_btn.click()
+                sleep(0.5)
+                basket_btn.click()
+                current, total = self.getCondition()
+                cnt += 1
+                if cnt == 150:
+                    self.driver.quit()
+                    return True
+
+            if total.text != current.text:
+                btn_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.index) + \
+                         '_cell_' + str(self.index) + '_0_controlbuttonTextBoxElement'
+                btn = self.driver.find_element_by_id(btn_id)
+                btn.click()
+                return False
+            else:
+                return True
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ stu_no = input('stu_no: ')
 pw = input('pw: ')
 grade = int(input('grade: '))
 index = int(input('index: '))
+method = int(input('method(0:전공, 1:장바구니): '))
 
 flag = True
 while flag:
-    macro = Macro(stu_no, pw, grade, index)
+    macro = Macro(stu_no, pw, grade, index, method)
     try:
         flag = macro.run()
     except:
